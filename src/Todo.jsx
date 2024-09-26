1c834d10f2bc8d974ac8f6bd5af37aafd706c257
yejinkill <jeanne842677@gmail.com>
2024-09-27 04:06:09 +0900

    feat : ToDo List div추가 컴포넌트 생성으로 코드 변경 / 입력내용 안들어가는 이슈 발생

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,48 +1,56 @@
 import './App.css';
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 
-function Todo (){
+const NewComponent = ({id,onDelete,onGetValue}) => {
+    console.log(onGetValue);
 
+    return <>
+        <div className="addDiv">
+            <div className="addTitle">{onGetValue}</div>
+            <button className="Delbtn" onClick={()=>onDelete(id)}>삭제</button>
+        </div>
+    </>
+};
+
+
+function Todo() {
     const [mkDiv, setMkDiv] = useState([]);
+    const [inputVal, setInputVal] = useState("");
+    const inputToDo = useRef(null);
 
-    const doDelDiv = (e) => {
-        console.log(e.target.id);
-        const IDval = e.target.id;
-        var regex = /[^0-9]/g;
-        const indexVal = IDval.replace(regex,"");
-        console.log(indexVal);
-        const newMkDiv = [...mkDiv];
-        newMkDiv.splice(indexVal,1);
-        setMkDiv(newMkDiv);
-        console.log(mkDiv);
+    const doDelDiv = (id) => {
+        setMkDiv(mkDiv.filter((mkDiv)=> mkDiv.id !== id));
+
     }
     const Clickbtn = () => {
-        setMkDiv([...mkDiv,
-        <div id={`addDiv-${mkDiv.length}`} key={`addDiv-${mkDiv.length}`} className="addDiv" >
-            <div id={`addTitle-${mkDiv.length}`} key={`addTitle-${mkDiv.length}`} className="addTitle">insert todo ${mkDiv.length}</div>
-            <button id={`Delbtn-${mkDiv.length}`} key={`Delbtn-${mkDiv.length}`} className="Delbtn"
-                onClick={doDelDiv}>삭제</button>
-        </div>])
-        console.log(setMkDiv);
-        console.log(mkDiv);
-        console.log(mkDiv.length);
+        const newComponentNum = {id: mkDiv.length+1};
+        setMkDiv([...mkDiv, newComponentNum]);
+        setInputVal("");
     }
+
+    const getValue = () => {
+        console.log(inputVal); // 현재 input 값 출력
+    };
 
 
     return(
         <div id="body1">
             <div id="section1">
-                <input id="inputbox" type="text"></input>
+                <input id="inputbox" type="text" value={inputVal} ref={inputToDo} onChange={(e) => {setInputVal(e.target.value)}}></input>
                 <button onClick={Clickbtn} id="addbtn">추가</button>
             </div>
             <div id="section2">
                 <div id="title">Todo List!</div>
             </div>
             <div id="section3" >
-                {mkDiv}
+                {mkDiv.map((mkDiv)=>(
+                    <NewComponent key={mkDiv.id} id={mkDiv.id} onGetValue={getValue} onDelete={doDelDiv} ></NewComponent>
+                ))}
             </div>
         </div>
     )
 }
+
+
 
 export default Todo;
