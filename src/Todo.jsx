225293af4b7cdf67168a35fed0a42a6a9e617684
yejinkill <jeanne842677@gmail.com>
2024-10-01 16:50:07 +0900

    feat : ToDo List 기본 기능 구현 끝!

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -16,8 +16,6 @@ function Todo() {
 
            setMkDiv([...mkDiv, inputVal])
             setInputVal("")
-        console.log(mkDiv);
-
     }
 
 
@@ -27,21 +25,20 @@ function Todo() {
                 <div className="addTitle">{props.item}</div>
                 <button className="Delbtn" onClick={()=> doDelDiv(props.index)}>삭제</button>
             </div>
+
         )
     }
 
     function doDelDiv(index){
-        console.log(index);
        const newMkDiv = [...mkDiv];
         newMkDiv.splice(index,1);
         setMkDiv(newMkDiv);
-
     }
 
     function AddBoard(props){
         return(
             <>
-                {props.mkDiv.map((item,index) => <AddItem key={index} item={item}/>)}
+                {props.mkDiv.map((item,index) => <AddItem key={index} index={index} item={item}/>)}
             </>
         )
     }
