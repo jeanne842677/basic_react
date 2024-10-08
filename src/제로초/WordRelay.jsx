c055bee5312d2135935034e27d629bba8fc55d25
yejinkill <jeanne842677@gmail.com>
2024-10-09 03:51:38 +0900

    feat : 끝말잇기 강의코드와 비교

--- src/제로초/WordRelay.jsx
+++ src/제로초/WordRelay.jsx
@@ -1,15 +1,43 @@
-const React = require('react');
-const { Component } = React;
-class WordRelay extends React.Component {
-    state ={
-        text: 'Hello, webpack',
-    };
+import React, {useRef, useState} from "react";
 
-    // eslint-disable-next-line react/require-render-return
-    render(){
-        return <h1>{this.state.text}</h1>;
+const WordRelay =()=>{
+    const [word, setWord] = useState('제로초');
+    const [value, setValue] = useState('');
+    const [result,setResult] = useState('');
+    const inputRef = useRef(null);
 
+    const onSubmitForm = (e) =>{
+        e.preventDefault();
+        if(word[word.length -1] === value[0]){
+            setResult('딩동댕');
+            setWord(value);
+            value('');
+            inputRef.current.focus();
+        }else{
+            setResult('땡');
+            setValue('');
+            inputRef.current.focus();
+        }
     }
+
+    const onChangeInput = (e) =>{
+        setValue(e.target.value);
+    }
+
+    return(
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputRef} value={value} onChange={onChangeInput}/>
+                <button >클릭!!!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    )
 }
+
+
+
+
 
 module.exports = WordRelay;
