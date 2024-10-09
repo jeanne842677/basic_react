30d84f19e456ec777d1c01e834393b2f688ec2e2
yejinkill <jeanne842677@gmail.com>
2024-10-10 03:27:43 +0900

    feat : 숫자야구 강의코드 참고 후 최종 수정

--- src/NumberBaseBall.jsx
+++ src/NumberBaseBall.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, {useState} from "react";
 
 const getNumbers = () => {
     const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -12,13 +12,11 @@ const getNumbers = () => {
 
 function NumberBaseBall () {
 
-    const [chosenNum, setChosenNum] = useState(getNumbers());
+
     const [result, setResult] = useState("");
-    const [inputValue, setInputValue] = useState("");
-    const refValue = useRef(null);
+    const [inputValue, setInputValue] = useState("")
+    const [chosenNum, setChosenNum] = useState(getNumbers);
     const [tryArray, setTryArray] = useState([]);
-
-
 
     const onSubmitForm = (e) =>{
         e.preventDefault();
@@ -30,17 +28,22 @@ function NumberBaseBall () {
         }
         if(strikeCnt === 4){
             setResult("홈런!!!!⚾️⚾️⚾️⚾️");
-            setChosenNum(getNumbers());
             alert("게임을 다시 시작합니다.");
+            setChosenNum(getNumbers());
             setTryArray([]);
+            setInputValue("");
+
         }else{
             setResult(strikeCnt + "스트라이크" + (4-strikeCnt) + "볼" );
-            setTryArray([...tryArray, inputValue]);
+            setTryArray([...tryArray, {try: tryArray.length+1, tryValue: inputValue, result: strikeCnt + "스트라이크" + (4-strikeCnt) + "볼"}]);
+            setInputValue("");
 
             if (tryArray.length === 10){
-                setChosenNum(getNumbers());
+                setResult("10번 넘게 틀려서 실패! 답은 " + chosenNum.join(',') + "이었습니다");
                 alert("게임을 다시 시작합니다.");
+                setChosenNum(getNumbers());
                 setTryArray([]);
+                setInputValue("");
 
             }
         }
@@ -53,10 +56,18 @@ function NumberBaseBall () {
             <div>{chosenNum}</div>
             <div>{result}</div>
             <form onSubmit={onSubmitForm} >
-                <input type="number" maxLength={4} value={inputValue} ref={refValue} onChange={(e)=>setInputValue(e.target.value)}/>
+                <input type="number" maxLength={4} value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
                 <button>입력!</button>
             </form>
-            <div>{tryArray.map((v,i)=> {return (<div key={v-i}>{v}</div>)})}</div>
+            <div>시도: {tryArray.length}</div>
+            <div>{tryArray.map((v,i)=> {
+                return (
+                    <>
+                        <div>{v.tryValue}</div>
+                        <div>{v.result}</div>
+                    </>
+                )
+            })}</div>
         </>
     )
 }
